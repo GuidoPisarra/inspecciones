d9e8d6acd7c33eb546480570178a1c149195ba13
pisarraguido <pisarraguido@gmail.com>
2025-01-21 19:34:20 -0300

    store

--- src/app/features/components/finish/finish.component.ts
+++ src/app/features/components/finish/finish.component.ts
@@ -3,40 +3,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ImagenVehiculo } from '../camera-capture/models/ImagenVehiculo';
 import { Store } from '@ngrx/store';
 import { selectImagenVehiculo } from '../camera-capture/store/selectors/imagen-vehiculo.selector';
-import { map, Observable } from 'rxjs';
-
-
-interface ImagenVehiculoView {
-  url: string;
-  confianza: number;
-  clase: string;
-}
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-finish',
   standalone: false,
+
   templateUrl: './finish.component.html',
   styleUrl: './finish.component.scss'
 })
 export class FinishComponent {
 
-  imagenesVehiculo$: Observable<ImagenVehiculoView[]>;
+  imagenesVehiculo$: Observable<ImagenVehiculo[]>;
 
   constructor(
-    private router: Router,
-    private route: ActivatedRoute,
+
     private store: Store
   ) {
-    this.imagenesVehiculo$ = this.store.select(selectImagenVehiculo).pipe(
-      map(imagenes => imagenes.map(imagen => ({
-        url: imagen.getArchivo()?.toString() ?? '',
-        confianza: imagen.getConfianza() ?? 0,
-        clase: imagen.getClase() ?? ''
-      })))
-    );
+
+    this.imagenesVehiculo$ = this.store.select(selectImagenVehiculo);
   }
 
   ngOnInit(): void {
-
+    this.imagenesVehiculo$.subscribe((imagen) => {
+      console.log(imagen);
+    });
   }
 }
